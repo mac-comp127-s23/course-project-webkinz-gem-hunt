3e333ee6ae20d1f2a0fe9c5f7a264a62bc53867b
ahiveley <123026656+ahiveley@users.noreply.github.com>
2023-04-11 13:00:05 -0500

    debugged Mine class

--- src/Mine.java
+++ src/Mine.java
@@ -10,35 +10,29 @@ public class Mine {
     private static double CANVAS_WIDTH = 800; // change this to pull from canvas settings in game class
     private static double CANVAS_HEIGHT = 600; // change this to pull from canvas settings in game class
 
+
     public Mine() {
-        GraphicsGroup group = new GraphicsGroup();
+        group = new GraphicsGroup();
+        addCave(300);
     }
 
     public static GraphicsGroup getGraphicsGroup() {
         return group;
     }
 
-    // for testing:
-    public static void main(String[] args) {
-        CanvasWindow canvas = new CanvasWindow("Mine", 800, 600);
-
-        addCave(50);
-        canvas.add(group);
-        canvas.draw();
-    }
-
     /**
-     * Creates a stalactites and stalagmites.
+     * Adds stalactites and stalagmites to GraphicsGroup
      *
-     * @param size   The height of each layer of cave rock, and width of each triangle
+     * @param size The height of each layer of cave rock, and width of each triangle
      * 
      */
     private static void addCave(double size) {
-        int layers = 4; // can adjust this or take as input parameter
+        int layers = 3; // can adjust this or take as input parameter
 
-        for (int layer = layers - 1; layer >= 0; layer--) {
-            group.add(createStalagmites(layer * size * 0.2)); // each layer is smaller/shorter according to the layer
-            group.add(createStalactites(layer * size * 0.2)); 
+        for (int layer = layers ; layer > 0; layer--) {
+            group.add(createStalagmites(layer * size * 0.2), 0, 0); // each layer is smaller/shorter according to the layer
+            group.add(createStalactites(layer * size * 0.2), 0, 0); 
+
         }
     }
 
@@ -48,16 +42,16 @@ public class Mine {
      * @param size The maximum height of the peaks
      */
     private static GraphicsGroup createStalagmites(double size) {
-        GraphicsGroup group = new GraphicsGroup();
+        GraphicsGroup mitesGroup = new GraphicsGroup();
 
         double layerLeft = Helpers.randomDouble(-size, 0);
         double layerRight = CANVAS_WIDTH + size;
-        Color layerColor = Helpers.randomColorVariation(Color.BLUE, 16); // change color to variable based upon instance var
+        Color layerColor = Helpers.randomColorVariation(Color.BLUE, Helpers.randomInt(30, 50)); // change color to variable based upon instance var
 
         double x = layerLeft;
         while (x < layerRight) {
             double curHeight = Helpers.randomDouble(size * 0.4, size),
-                curWidth = curHeight * Helpers.randomDouble(1.0, 1.6);
+                curWidth = curHeight * 0.2*Helpers.randomDouble(1.0, 1.6);
             Path peak = Path.makeTriangle(
                 x - curWidth, CANVAS_HEIGHT,
                 x, CANVAS_HEIGHT - curHeight,
@@ -65,10 +59,11 @@ public class Mine {
             peak.setFillColor(layerColor);
             peak.setFilled(true);
             peak.setStroked(false);
+            mitesGroup.add(peak);
             group.add(peak);
             x += curWidth * 0.5;
         }
-        return group;
+        return mitesGroup;
     }
 
     /**
@@ -77,16 +72,16 @@ public class Mine {
      * @param size The maximum height of the peaks
      */
     private static GraphicsGroup createStalactites(double size) {
-        GraphicsGroup group = new GraphicsGroup();
+        GraphicsGroup titesGroup = new GraphicsGroup();
 
         double layerLeft = Helpers.randomDouble(-size, 0);
         double layerRight = CANVAS_WIDTH + size;
-        Color layerColor = Helpers.randomColorVariation(Color.BLUE, 16); // change color to variable based upon instance var
+        Color layerColor = Helpers.randomColorVariation(Color.BLUE, Helpers.randomInt(30, 50)); // change color to variable based upon instance var
 
         double x = layerLeft;
         while (x < layerRight) {
             double curHeight = Helpers.randomDouble(size * 0.4, size),
-                curWidth = curHeight * Helpers.randomDouble(1.0, 1.6);
+                curWidth = curHeight * 0.2*Helpers.randomDouble(1.0, 1.6);
             Path peak = Path.makeTriangle(
                 x - curWidth, 0,
                 x, curHeight,
@@ -94,10 +89,21 @@ public class Mine {
             peak.setFillColor(layerColor);
             peak.setFilled(true);
             peak.setStroked(false);
-            group.add(peak);
+            titesGroup.add(peak);
             x += curWidth * 0.5;
         }
-        return group;
+        return titesGroup;
+    }
+
+    
+    // for testing:
+    public static void main(String[] args) {
+        group = new GraphicsGroup();
+        CanvasWindow canvas = new CanvasWindow("Mine", 800, 600);
+
+        addCave(300);
+        canvas.add(group);
+        canvas.draw();
     }
     
 }
