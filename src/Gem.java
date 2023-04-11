51fa6850a409701880b425b6b7c698bebf6e7eaf
Stephanie Miles <118240368+stephaniemiless@users.noreply.github.com>
2023-04-11 10:23:40 -0500

    added image files into resource folder

--- src/Gem.java
+++ src/Gem.java
@@ -1,8 +1,14 @@
+import edu.macalester.graphics.*;
+
 public class Gem {
     
     private String name;
     private String description;
     private int regLowPrice;
     private int regHighPrice;
-    
+    private int gemOfDayPrice;
+    private String type;
+    private double rarity;
+    private Image image;
+
 }
--- src/GemReader.java
+++ src/GemReader.java
@@ -6,6 +6,20 @@ import java.util.Scanner;
 public class GemReader {
   
     public static void main(String[] args) {
+        try {
+            File file = new File("res/gem_descriptions.csv");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+            scanner.close();
+        }
+
+        catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
         
     }
     
