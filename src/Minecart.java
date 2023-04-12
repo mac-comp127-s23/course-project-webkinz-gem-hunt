835245a3b83a972251cae3dec9f9453b289c5f39
Stephanie Miles <118240368+stephaniemiless@users.noreply.github.com>
2023-04-12 10:56:11 -0500

    added wheels to minecart (working on figuring out path for lump of slag on top)

--- src/Minecart.java
+++ src/Minecart.java
@@ -23,6 +23,16 @@ public class Minecart {
         cart.setCenter(400, 550);
         minecart.add(cart);
 
+        Ellipse wheel1 = new Ellipse(0, 0, 30, 30);
+        wheel1.setFillColor(Color.GRAY);
+        wheel1.setCenter(380, 590);
+        minecart.add(wheel1);
+
+        Ellipse wheel2 = new Ellipse(0, 0, 30, 30);
+        wheel2.setFillColor(Color.GRAY);
+        wheel2.setCenter(420, 590);
+        minecart.add(wheel2);
+
         return minecart;
     }
 
